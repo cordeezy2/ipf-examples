2445fcce9a1c862f913de723782a990ef6ecfc9d
Cristian Cordero <cristian.cordero@protonmail.com>
2024-08-16 13:38:52 -0400

    add staging page for snow

---
+++ st_pages/service_now_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def service_now_page():
+    st.title("Service Now")
+    st.write("built using Streamlit [docs.streamlit.io](https://docs.streamlit.io/).")
+    st.write("This page is under construction.")
+    st.write("Please select a different page from the sidebar.")
+    st.write("Thank you for your patience.")
